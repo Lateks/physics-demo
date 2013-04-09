9dd2174f80cb16fe0e3759a11b2929a8dae43e06
Laura Leppänen <laura.leppanen@iki.fi>
2013-04-09 15:51:03 +0300

    Add bullet and get rid of custom collision library projects.

--- CollisionLib/CollisionMath.h
+++
@@ -1,7 +0,0 @@
-#ifndef COLLISION_MATH_H
-#define COLLISION_MATH_H
-#include "CollisionShape.h"
-
-bool GJKCollide(const ICollisionShape& shape1, const ICollisionShape& shape2);
-
-#endif
--- CollisionLib/SphereShape.cpp
+++
@@ -1,9 +0,0 @@
-#include "SphereShape.h"
-#include "LinearAlgebraTypes.h"
-
-Point3D SphereShape::GetFarthestPointInDirection(const Vector3D& direction) const
-{
-	Vector3D nd = direction.normalize();
-	Point3D location = _gameWorldObject->GetWorldLocation();
-	return location + _radius * nd;
-}

c53419a8f822834cf174482faa147b111f99ceb8
Laura Leppänen <laura.leppanen@iki.fi>
2013-04-10 22:35:47 +0300

    Use RapidXML to parse the XML data.
    Currently there seems to be a problem with the auto pointers etc.
    that are used because something gets freed twice. Must fix.

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -2,9 +2,28 @@
 //
 
 #include "Game.h"
+#include "XMLPhysicsData.h"
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void dump_map(const std::map<string, float> map) {
+  for ( std::map<string,float>::const_iterator it = map.begin(); it != map.end(); it++) {
+    cout << "Key: " << it->first << endl;
+    cout << "Val: " << it->second << endl;
+  }
+}
 
 int main()
 {
+	GameEngine::PhysicsEngine::XMLPhysicsData data;
+	data.LoadDataFromXML("..\\assets\\materials.xml");
+
+	dump_map(data.m_densities);
+
 	GameEngine::Game game;
 
 	return game.Run();
--- Demo/XMLPhysicsData.cpp
+++ Demo/XMLPhysicsData.cpp
@@ -1,13 +1,113 @@
 #include "XMLPhysicsData.h"
+#include <rapidxml.hpp>
 #include <stdexcept>
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <memory>
+#include <functional>
+
+using rapidxml::xml_document;
+using rapidxml::xml_node;
+using rapidxml::xml_attribute;
+
+using std::string;
+using std::stof;
+
+using std::make_pair;
+
+using std::ifstream;
+using std::istreambuf_iterator;
+
+typedef std::shared_ptr<xml_node<>> SharedNodePtr;
 
 namespace GameEngine
 {
 	namespace PhysicsEngine
 	{
+		float ReadFloatAttribute(SharedNodePtr node, const string& attributeName)
+		{
+			std::auto_ptr<xml_attribute<>> attr;
+			std::auto_ptr<const char> value;
+			value.reset(attributeName.c_str());
+
+			attr.reset(node->first_attribute(value.get()));
+			assert(attr.get());
+			value.reset(attr->value());
+			return stof(value.get());
+		}
+
+		std::pair<string, MaterialData> ReadMaterialData(SharedNodePtr node)
+		{
+			float restitution;
+			float friction;
+			std::auto_ptr<xml_attribute<>> attr;
+			
+			restitution = ReadFloatAttribute(node, "restitution");
+			friction = ReadFloatAttribute(node, "friction");
+
+			std::auto_ptr<char> name;
+			name.reset(node->name());
+
+			return make_pair(string(name.get()), MaterialData(restitution, friction));
+		}
+
+		std::pair<string, float> ReadDensity(SharedNodePtr node)
+		{
+			float density;
+			std::auto_ptr<xml_node<>> n;
+			std::auto_ptr<char> value;
+
+			n.reset(node->first_node());
+			assert(n.get());
+			value.reset(n->value());
+			density = stof(value.get());
+
+			value.reset(node->value());
+			return make_pair(string(value.get()), density);
+		}
+
+		void MapChildNodes(std::shared_ptr<xml_node<>> inputNode,
+			std::function<void(SharedNodePtr)> handleNode)
+		{
+			std::shared_ptr<xml_node<>> node;
+			for (node.reset(inputNode->first_node());
+				 node.get();
+				 node.reset(node->next_sibling()))
+			{
+				handleNode(node);
+			}
+		}
+
 		void XMLPhysicsData::LoadDataFromXML(const std::string& fileName)
 		{
-			// TODO
+			ifstream matFile(fileName);
+			string matData(
+				(istreambuf_iterator<char>(matFile)),
+				(istreambuf_iterator<char>()));
+			xml_document<> doc;
+			std::auto_ptr<const char> content;
+			content.reset(matData.c_str());
+			doc.parse<0>(const_cast<char*>(content.get()));
+			std::auto_ptr<xml_node<>> root;
+			root.reset(doc.first_node());
+
+			SharedNodePtr materialNode;
+			materialNode.reset(root->first_node("PhysicsMaterials"));
+			assert(materialNode.get());
+
+			MapChildNodes(materialNode, [this] (SharedNodePtr node)
+			{
+				this->m_materialTable.insert(ReadMaterialData(node));
+			});
+
+			materialNode.reset(root->first_node("DensityTable"));
+			assert(materialNode.get());
+
+			MapChildNodes(materialNode, [this] (SharedNodePtr node)
+			{
+				m_densities.insert(ReadDensity(node));
+			});
 		}
 
 		MaterialData XMLPhysicsData::LookupMaterial(const std::string& materialName)

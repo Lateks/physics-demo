9f354442c610f3a947b10d7d7d7ff33058fc954b
Laura Leppänen <laura.leppanen@iki.fi>
2013-04-28 22:59:32 +0300

    Integrate IrrlichtMessagingWindow into IrrlichtDisplay.cpp.

--- Demo/IrrlichtDisplay.cpp
+++ Demo/IrrlichtDisplay.cpp
@@ -1,6 +1,6 @@
 #include "IrrlichtDisplay.h"
 #include "IrrlichtInputState.h"
-#include "IrrlichtMessagingWindow.h"
+#include "MessagingWindow.h"
 #include "GameActor.h"
 #include "WorldTransformComponent.h"
 #include "GameData.h"
@@ -67,6 +67,31 @@ namespace GameEngine
 			void UpdateActorPosition(Events::EventPtr pEvent);
 			void SetNodeTransform(irr::scene::ISceneNode *pNode, shared_ptr<WorldTransformComponent> pWorldTransform);
 			unsigned int GetTime();
+		};
+
+		class IrrlichtMessagingWindow : public MessagingWindow
+		{
+		public:
+			IrrlichtMessagingWindow(std::size_t messageBufferSize, irr::gui::IGUIEnvironment *irrlichtGUI);
+			~IrrlichtMessagingWindow();
+			virtual void AddMessage(const std::wstring& message) override;
+			virtual void SetPosition(unsigned int minX, unsigned int minY) override;
+			virtual void SetWidth(unsigned int width) override;
+			virtual void SetFont(const std::string& fontFileName) override;
+			virtual void SetVisible(bool visible) override;
+			virtual void Render() const override;
+		private:
+			irr::gui::IGUIFont *m_pFont;
+			irr::gui::IGUIEnvironment *m_irrlichtGUI;
+			bool m_visible;
+			unsigned int m_posX;
+			unsigned int m_posY;
+			unsigned int m_width;
+			unsigned int m_fontHeight;
+			unsigned int m_maxMessages;
+			std::vector<irr::core::stringw> m_messageBuffer;
+			irr::video::SColor m_color;
+			void SetFontHeight();
 		};
 
 		// Conversions between Irrlicht linear algebra types and the game engine's
@@ -432,6 +457,79 @@ namespace GameEngine
 
 			pNode->setPosition(ConvertVector(pWorldTransform->GetPosition()));
 			pNode->setScale(ConvertVector(pWorldTransform->GetScale()));
+		}
+
+				IrrlichtMessagingWindow::IrrlichtMessagingWindow(std::size_t messageBufferSize, irr::gui::IGUIEnvironment *irrlichtGUI)
+			: m_color(255, 255, 255, 255), m_maxMessages(messageBufferSize),
+			m_posX(0), m_posY(0), m_visible(false), m_pFont(nullptr), m_width(400),
+			m_irrlichtGUI(irrlichtGUI)
+		{
+			if (m_irrlichtGUI)
+			{
+				m_pFont = m_irrlichtGUI->getBuiltInFont();
+				SetFontHeight();
+			}
+		}
+
+		void IrrlichtMessagingWindow::SetFontHeight()
+		{
+			if (m_pFont)
+			{
+				m_fontHeight = m_pFont->getDimension(L"I").Height + 2;
+			}
+		}
+
+		IrrlichtMessagingWindow::~IrrlichtMessagingWindow() { }
+
+		void IrrlichtMessagingWindow::AddMessage(const std::wstring& message)
+		{
+			if (m_messageBuffer.size() == m_maxMessages)
+			{
+				m_messageBuffer.erase(m_messageBuffer.begin());
+			}
+			m_messageBuffer.push_back(irr::core::stringw(message.c_str()));
+		}
+
+		void IrrlichtMessagingWindow::SetPosition(unsigned int minX, unsigned int minY)
+		{
+			m_posX = minX;
+			m_posY = minY;
+		}
+
+		void IrrlichtMessagingWindow::SetFont(const std::string& fontFilePath)
+		{
+			if (m_irrlichtGUI)
+			{
+				m_pFont = m_irrlichtGUI->getFont(fontFilePath.c_str());
+				SetFontHeight();
+			}
+		}
+
+		void IrrlichtMessagingWindow::SetVisible(bool visible)
+		{
+			m_visible = visible;
+		}
+
+		void IrrlichtMessagingWindow::SetWidth(unsigned int width)
+		{
+			m_width = width;
+		}
+
+		void IrrlichtMessagingWindow::Render() const
+		{
+			assert(m_pFont);
+			if (!m_pFont || !m_visible)
+				return;
+
+			unsigned int x = m_posX;
+			unsigned int y = m_posY;
+			for (std::size_t i = 0; i < m_messageBuffer.size(); i++)
+			{
+				m_pFont->draw(
+					m_messageBuffer[i], irr::core::rect<irr::s32>(
+					x, y, x + m_width, y + m_fontHeight), m_color);
+				y += m_fontHeight;
+			}
 		}
 	}
 }

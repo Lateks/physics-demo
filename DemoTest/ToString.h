992d0a43a6bde5c3aac4d42ccbdf43d68224fe85
Laura Leppänen <laura.leppanen@iki.fi>
2013-05-10 22:31:33 +0300

    s/spaces/tabs.

--- DemoTest/ToString.h
+++ DemoTest/ToString.h
@@ -2,13 +2,13 @@
 #include <Vec3.h>
 
 namespace Microsoft
-{ 
-    namespace VisualStudio
-    { 
-        namespace CppUnitTestFramework
-        {
+{
+	namespace VisualStudio
+	{
+		namespace CppUnitTestFramework
+		{
 			template <> static std::wstring ToString<GameEngine::Vec3>(const GameEngine::Vec3& t) { RETURN_WIDE_STRING(t); }
 			template <> static std::wstring ToString<GameEngine::CSHandedness>(const GameEngine::CSHandedness& t) { RETURN_WIDE_STRING(t); }
-        }
-    }
+		}
+	}
 }
